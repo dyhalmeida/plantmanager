0b197176c5d922ad66dc9bb82fcc8b83f2f22fae
Diego Almeida <dyhalmeida@gmail.com>
2021-05-12 16:22:59 -0300

    :lipstick: add safe area

---
+++ src/screens/UserIdentification/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+const UserIdentification = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'space-around',
+  },
+});
+
+export { UserIdentification };
