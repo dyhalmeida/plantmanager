731f8bde5aeeb11ae4928abacb8707dcebe62f31
Diego Almeida <dyhalmeida@gmail.com>
2021-05-12 14:58:49 -0300

    :lipstick: add global fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import {
+  useFonts,
+  Jost_600SemiBold,
+  Jost_400Regular,
+} from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
+
 import { Welcome } from './src/screens/Welcome';
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Jost_600SemiBold,
+    Jost_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Welcome />;
 };
 
