014b9282f82ba374ec7c594413530e04c8bb7a77
Diego Almeida <dyhalmeida@gmail.com>
2021-05-14 09:51:11 -0300

    :lipstick: add safe area view

---
+++ src/screens/ConfirmationUser/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+const ConfirmationUser = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});
+
+export { ConfirmationUser };
