507d65be4b722f8904fa6cd48638df6679546318
Diego Almeida <dyhalmeida@gmail.com>
2021-05-14 09:55:36 -0300

    :lipstick: add button to footer view

--- src/screens/ConfirmationUser/index.tsx
+++ src/screens/ConfirmationUser/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import { Button } from '../../components/Button';
 import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 
@@ -12,6 +13,9 @@ const ConfirmationUser = () => {
         <Text style={styles.subtitle}>
           Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
         </Text>
+        <View style={styles.footer}>
+          <Button />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -47,6 +51,11 @@ const styles = StyleSheet.create({
     fontSize: 17,
     paddingHorizontal: 10,
     color: colors.heading,
+  },
+  footer: {
+    width: '100%',
+    paddingHorizontal: 50,
+    marginTop: 20,
   },
 });
 
