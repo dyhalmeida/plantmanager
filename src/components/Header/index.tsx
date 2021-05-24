a58d23864773793b200b58578f34d7867c31d6c2
Diego Almeida <dyhalmeida@gmail.com>
2021-05-24 15:52:00 -0300

    :lipstick: (Header) add: welcome to user

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
+
 const Header = () => {
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.username}>Diego</Text>
+      </View>
     </View>
   );
 };
@@ -15,6 +23,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
     marginTop: getStatusBarHeight(),
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  username: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
   },
 });
 
