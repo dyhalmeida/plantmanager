966e2c7a8df6f240f05a369365d21e0cd1f0bcda
Diego Almeida <dyhalmeida@gmail.com>
2021-05-28 08:12:06 -0300

    :sparkles: add environment button list and request api

--- src/screens/PlantSelect/index.tsx
+++ src/screens/PlantSelect/index.tsx
@@ -3,12 +3,37 @@ import {
   StyleSheet,
   View,
   Text, 
+  FlatList,
 } from 'react-native';
+import { EnvironmentButton } from '../../components/EnvironmentButton';
 import { Header } from '../../components/Header';
+import { api } from '../../services/api';
 import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 
+interface EnvironmentProps {
+  key: string;
+  title: string;
+}
 const PlantSelect = () => {
+  const [environments, setEnvironments] = React.useState<Array<EnvironmentProps>>([]);
+  const [environmentSelected, setEnvironmentSelected] = React.useState('all');
+
+  React.useEffect(() => {
+
+    (async () => {
+      const { data } = await api.get('plants_environments?_sort=title&_order=asc');
+      setEnvironments([
+        { key: 'all', title: 'Todos'},
+        ...data
+      ]);
+    })();
+
+  }, []);
+  function handleEnvironmentSelected(environment: string) {
+    setEnvironmentSelected(environment);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -17,6 +42,20 @@ const PlantSelect = () => {
         <Text style={styles.subtitle}>você quer colocar a sua planta?</Text>
     </View>
       <View>
+        <FlatList
+          data={environments}
+          keyExtractor={(item) => String(item.key)}
+          renderItem={({ item }) => (
+            <EnvironmentButton 
+              title={item.title} 
+              active={item.key === environmentSelected} 
+              onPress={() => handleEnvironmentSelected(item.key)}
+            />
+          )}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.environmentList}
+        />
       </View>
       <View style={styles.plants}>
       </View>
@@ -42,6 +81,13 @@ const styles = StyleSheet.create({
     fontSize: 17,
     lineHeight: 20,
     color: colors.heading,
+  },
+  environmentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
   },
   plants: {
     flex: 1,
