e7aafa4b515d097fc058a6859e474ba3b8d71e61
Diego Almeida <dyhalmeida@gmail.com>
2021-05-28 08:08:56 -0300

    :sparkles: add PlantSelect View

--- src/screens/PlantSelect/index.tsx
+++ src/screens/PlantSelect/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { 
+  StyleSheet,
+  View,
+  Text, 
+} from 'react-native';
 import { Header } from '../../components/Header';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
 const PlantSelect = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
       <Header />
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar a sua planta?</Text>
     </View>
+      <View>
+      </View>
+      <View style={styles.plants}>
+      </View>
   );
 };
 
@@ -14,6 +27,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+  plants: {
+    flex: 1,
+    paddingHorizontal: 32,
+    justifyContent: 'center'
+  }
 });
 
 export { PlantSelect };
