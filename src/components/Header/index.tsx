c0b557004e8a7719b5493683704f021c7bb8bec2
Diego Almeida <dyhalmeida@gmail.com>
2021-06-01 16:25:02 -0300

    :sparkles: add username from storage to header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { View, StyleSheet, Text, Image } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import ProfileImage from '../../assets/profile.jpeg';
 import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 
 const Header = () => {
+
+  const [username, setUsername] = React.useState<string>('');
+
+  async function getUsernameFromStorage() {
+    const username = await AsyncStorage.getItem('@plantmanager:username');
+    setUsername(username || '');
+  }
+
+  React.useEffect(() => {
+    getUsernameFromStorage();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.greeting}>Olá,</Text>
-        <Text style={styles.username}>Diego</Text>
+        <Text style={styles.greeting}>Olá</Text>
+        <Text style={styles.username}>{username}</Text>
       </View>
       <Image style={styles.image} source={ProfileImage} />
     </View>
